ced1aae7354feed51232f87ea24d13b2bddf55c1
bobowang2017 <anini456123>
2020-09-28 16:30:53 +0800

    优化项目目录结构

--- Controllers/userController.go
+++ Controllers/userController.go
@@ -1,8 +1,14 @@
 package Controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func UserListController(c *gin.Context) {
+	//user := Models.User{Name: "Jinzhu", Age: 18, Birthday: time.Now()}
+	//	//db.NewRecord(user) // => 主键为空返回`true`
+	//	//db.Create(&user)
+	//	//db.NewRecord(user) //
 	c.JSON(200, gin.H{
 		"message": "Apps",
 	})
--- Models/user.go
+++ Models/user.go
@@ -1,9 +1,14 @@
 package Models
 
-import "github.com/jinzhu/gorm"
+import "time"
 
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
+type User struct {
+	Id          int    `gorm:"AUTO_INCREMENT"` // 自增
+	Username    string `gorm:"size:64"`        // string默认长度为255, 使用这种tag重设。
+	Password    string `gorm:"size:64"`
+	Name        string `gorm:"size:16"`
+	Sex         int
+	Birthday    time.Time
+	CreatedTime time.Time
+	UpdatedTime time.Time
 }
