f1e355ff8eda5dbdcaa2bde788520752c34bb136
bobowang2017 <409879654@qq.com>
2020-11-10 20:43:13 +0800

    参数验证demo

--- models/user.go
+++ models/user.go
@@ -6,9 +6,9 @@ import (
 
 type User struct {
 	BaseModel
-	Username string `gorm:"size:64;not null"` // string默认长度为255, 使用这种tag重设。
-	Password string `gorm:"size:64;not null"`
-	Name     string `gorm:"size:16;default:'hello world'"`
+	Username string `gorm:"size:64;not null" json:"username" binding:"required"` // string默认长度为255, 使用这种tag重设。
+	Password string `gorm:"size:64;not null" json:"password" binding:"required,min=6,max=12"`
+	Name     string `gorm:"size:16;default:'hello world'" json:"name"`
 	Sex      int    `gorm:"default:0"`
 	Birthday time.Time
 }
